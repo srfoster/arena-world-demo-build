95d2d696158e9d309651af9bb7a0591fb41f701f
Stephen Foster <stephen@thoughtstem.com>
2020-11-10 13:06:22 -0800

    updating to conventions

--- lore.rkt
+++ lore.rkt
@@ -1,31 +1,29 @@
 #lang at-exp racket
 
-(require fire-particles/lore
-	 website-js)
+(require ;fire-particles/lore
+	 website-js
+	 racket/runtime-path)
 
 (provide name
          description
          rune-collections
-         )
+	 preview-image)
 
 (define (name)
-  "Arena World"
-  )
+  "Arena World")
 
 (define (description)
   @md{
     This work is an Arena.  
-  })
+
+    @(div
+      (alert-warning (b "Note. ") "We are currently adding new runes and will update this world when we do.")
+      (alert-primary "In its current form, we estimate this build to be worth about " (b "3 minutes of fun") ". Try filling the arena with particles."))
+    })
 
 (define (rune-collections)
   (list
-    @md{
-      @(a href: "/collections/illusions-of-flame/index.html" "Illusions of Flame")}
-    )
-  )
+    'fire-particles))
 
-(module+ main
-	 (render #:to "out" 
-		 (list
-		   (bootstrap-files)
-		   (lore))))
+(define-runtime-path preview-image "images/preview.png")
+
