f992dd7ff4bda22fdf11f9ca99261ff7fde44342
srfoster <lindseyhandley@gmail.com>
2020-11-10 14:10:16 -0800

    updated lore

--- lore.rkt
+++ lore.rkt
@@ -10,20 +10,20 @@
 	 preview-image)
 
 (define (name)
-  "Arena World")
+  "Arena")
 
 (define (description)
   @md{
     This work is an Arena.  
 
     @(div
-      (alert-warning (b "Note. ") "We are currently adding new runes and will update this world when we do.")
       (alert-primary "In its current form, we estimate this build to be worth about " (b "3 minutes of fun") ". Try filling the arena with particles."))
     })
 
 (define (rune-collections)
   (list
-    'fire-particles))
+    'fire-particles
+    ))
 
 (define-runtime-path preview-image "images/preview.png")
 
