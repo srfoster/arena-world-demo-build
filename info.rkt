c8754e44a44a2ba8cb572d65e6b1640f3afa13a5
Stephen Foster <stephen@thoughtstem.com>
2020-11-10 08:48:22 -0800

    removing scribblings

--- info.rkt
+++ info.rkt
@@ -2,7 +2,6 @@
 (define collection "arena-world-demo-build")
 (define deps '("base"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
-(define scribblings '(("scribblings/my-mod.scrbl" ())))
 (define pkg-desc "Description Here")
 (define version "0.0")
 (define pkg-authors '(ThoughtSTEM))
