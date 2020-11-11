e89ba5fec5a0c79d26bfdc3f0fcc4d4dfb174e4f
Stephen Foster <stephen@thoughtstem.com>
2020-11-11 10:04:30 -0800

    new lore

--- lore.rkt
+++ lore.rkt
@@ -1,29 +1,19 @@
 #lang at-exp racket
 
-(require ;fire-particles/lore
-	 website-js
-	 racket/runtime-path)
+(require codespells/lore)
 
-(provide name
-         description
-         rune-collections
-	 preview-image)
+(define-runtime-path preview-image "images/preview.png")
 
-(define (name)
-  "Arena World")
-
-(define (description)
+(define-authored-work-lore
+  #:name "Arena" 
+  #:description 
   @md{
     This work is an Arena.  
 
     @(div
       (alert-warning (b "Note. ") "We are currently adding new runes and will update this world when we do.")
       (alert-primary "In its current form, we estimate this build to be worth about " (b "3 minutes of fun") ". Try filling the arena with particles."))
-    })
-
-(define (rune-collections)
-  (list
-    'fire-particles))
-
-(define-runtime-path preview-image "images/preview.png")
-
+    }
+  #:rune-collections
+  (list 'fire-particles)
+  #:preview-image preview-image)
