ce7ea20ce5de47c37670ce1733844f45a39aa54d
srfoster <lindseyhandley@gmail.com>
2020-11-24 14:50:06 -0800

    fixed lore to explain limited world size

--- lore.rkt
+++ lore.rkt
@@ -7,7 +7,7 @@
   #:name "Arena" 
   #:description 
   @md{
-    Formerly known as the @b{Arena of the Golden Sun}, this Arena has been used by Wizards across the multiverse as a training ground for the development and testing of new Rune Collections.  
+    Formerly known as the @b{Arena of the Golden Sun}, this Arena has been used by Wizards across the multiverse since the @b{First Epoch} as a training ground for the development and testing of new Rune Collections. The world is large enough for most spell development.  
 
     @(div
       (alert-primary "In its current form, we estimate this build to be worth about " (b "3 minutes of fun") ". Try filling the arena with particles."))
